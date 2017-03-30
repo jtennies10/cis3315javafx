a039fcf3037f9c568105c5c71c2e960ef35cd3b1
Joshua Tennies <tenniesjd13@mounties.mansfield.edu>
2017-03-30 14:27:06 -0400

    JavaFX exercises

--- src/week8/Life.java
+++ src/week8/Life.java
@@ -1,24 +1,17 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package week8;
 
-
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
-import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 /**
+ * Program to run Conway's Game of Life simulation. This is just a rough draft
+ * to gain insight.
  *
  * @author Joshua Tennies
  */
@@ -32,7 +25,7 @@ public class Life extends Application {
     public void start(Stage primaryStage) {
         initCells();
         borderPane.setCenter(pane);
-        
+
         Button btn = new Button("Next Generation");
         btn.setOnAction(e -> {
             System.out.println("btn pressed");
@@ -40,85 +33,102 @@ public class Life extends Application {
             drawCells();
         });
         borderPane.setBottom(btn);
-        
-        Scene scene = new Scene(borderPane, 300, 250);
+
+        Scene scene = new Scene(borderPane, 800, 800);
 
         primaryStage.setTitle("Conway's Game of Life");
         primaryStage.setScene(scene);
         primaryStage.show();
-        
         drawCells();
     }
-    
+
+    public void initCells() {
+        for (int y = 0; y < 100; y++) {
+            for (int x = 0; x < 100; x++) {
+                cells[x][y] = Math.random() > 0.5 ? 0 : 1;
+            }
+        }
+    }
+
     public void drawCells() {
         pane.getChildren().clear();
-        for(int y = 0; y < 100; y++) {
-            for(int x = 0; x < 100; x++) {
-                Rectangle rect = new Rectangle(x*10, y*10, 9, 9);
+        for (int y = 0; y < 100; y++) {
+            for (int x = 0; x < 100; x++) {
+                Rectangle rect = new Rectangle(x * 10, y * 10, 9, 9);
                 rect.setFill(cells[x][y] == 0 ? Color.WHITE : Color.BLACK);
                 pane.getChildren().add(rect);
             }
         }
     }
-    
+
+    // decide who lives and dies in the next generation
     public void nextGen() {
         int[][] cellsng = new int[100][100];
-        for(int y = 0; y < 100; y++) {
-            for(int x = 0; x < 100; x++) {
+
+        // count how many living neighbors each cell has
+        for (int y = 0; y < 100; y++) {
+            for (int x = 0; x < 100; x++) {
+
                 int neighbors = 0;
-                if(x>1 && x<99 && y>1 && y<99) {
-                    if(cells[x-1][y-1] == 1) {
+                if (x > 1 && x < 99 && y > 1 && y < 99) {
+
+                    // check row above cell x,y
+                    if (cells[x - 1][y - 1] == 1) {
                         neighbors++;
                     }
-                    if(cells[x][y-1] == 1) {
+                    if (cells[x][y - 1] == 1) {
                         neighbors++;
                     }
-                    if(cells[x+1][y-1] == 1) {
+                    if (cells[x + 1][y - 1] == 1) {
                         neighbors++;
                     }
-                    
-                    if(cells[x-1][y] == 1) {
+
+                    // check neighbors left and right of cell x,y
+                    if (cells[x - 1][y] == 1) {
                         neighbors++;
                     }
-//                    if(cells[x][y] == 1) {
+//                    if (cells[x][y] == 1) {
 //                        neighbors++;
 //                    }
-                    if(cells[x+1][y+1] == 1) {
+                    if (cells[x + 1][y] == 1) {
                         neighbors++;
                     }
-                    
-                    if(cells[x-1][y+1] == 1) {
+
+                    // check neighbors below cell x,y
+                    if (cells[x - 1][y + 1] == 1) {
                         neighbors++;
                     }
-                    if(cells[x][y+1] == 1) {
+                    if (cells[x][y + 1] == 1) {
                         neighbors++;
                     }
-                    if(cells[x+1][y+1] == 1) {
+                    if (cells[x + 1][y + 1] == 1) {
                         neighbors++;
                     }
-                    if(cells[x][y] == 1) {
-                        if(neighbors < 2 || neighbors > 3) {
+
+                    // decide who lives or dies
+                    if (cells[x][y] == 1) {
+
+                        // check for loneliness or overcrowding
+                        if (neighbors < 2 || neighbors > 3) {
                             cellsng[x][y] = 0;
                         } else {
                             cellsng[x][y] = 1;
                         }
-                    } else if(neighbors == 3) {
+
+                        // check if dead cell should come alive
+                    } else if (neighbors == 3) {
                         cellsng[x][y] = 1;
-                    } else {
-                        cellsng[x][y] = 0;
                     }
+
+                    // kill edge cells
+                } else {
+                    cellsng[x][y] = 0;
                 }
             }
         }
+
+        // update the current generation to be the next generation
         cells = cellsng;
-    }
-    
-    public void initCells() {
-        for(int y = 0; y < 100; y++) {
-            for(int x = 0; x < 100; x++) {
-                cells[x][y] = Math.random() > 0.5 ? 0 : 1;
-            }
-        }
     }
 
     /**
